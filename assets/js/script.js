24bb6921ef5b3bc5f776089c36d4fabb605248d3
omer ahmed <omerah69@gmail.co>
2024-06-18 17:02:05 +0200

    added styling effects

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+let userChoice=document.getElementByIdID('user-choice');
+let computerChoice=document.getElementByIdID('computer-choice');
+let score=document.getElementByIdID('score');
+
+
+console.log(userChoice);
