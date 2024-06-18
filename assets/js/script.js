9d7d5e76ed49c2e0a88a5d91c7548fcfbfc0f129
omer ahmed <omerah69@gmail.co>
2024-06-18 20:24:47 +0200

    added constants in script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 let userChoice=document.getElementById('user-choice');
 let computerChoice=document.getElementById('computer-choice');
 let score=document.getElementById('score');
+let choices=document.querySelectorAll('button');
 
 
-console.log('userChoice');
+
+
+console.log('choices');
