e95b63c06585618169b4d59d779bf4f430ad4c4a
omer ahmed <omerah69@gmail.co>
2024-06-19 18:18:25 +0200

    fix playgame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,74 +7,55 @@ const spock=document.getElementById('spock');
 const userChoices=[rock, paper, scissor, lizard, spock];
 const computerChoices=['rock','paper','scissor','lizard','spock'];
 
-
 const score=document.getElementById('score');
-
 
 let  userScore=0;
 let computerScore=0;
 
 userChoices.forEach((choice,index) => {
     choice.addEventListener('click', () => playGame(computerChoices[index]));
-
 });
 
-
 function playGame(userChoice){
-    const random=Math.floor(Math.random()*computerChoices.length);
+    const random=Math.floor(Math.random() * computerChoices.length);
     const computerChoice= computerChoices[random];
     if (userChoice===computerChoice){
         score.textContent='its a draw';
-    }
+        }
         else if(userChoice==='rock'){
             if(computerChoice==='paper'){
                 score.textContent='Computer Won';
-                computerChoice++;computerChoice;
             }else {
                 score.textContent='User Won';
                 userScore++;
-                userChoice++;userChoice;
-                
             }
         }
         else if(userChoice==='paper'){
             if (computerChoice==='scissors'){
                 score.textContent='Computer Won';
-                
             }else{
                 score.textContent='User Won';
                 userScore++;
-                userChoice++;userChoice;
-
             }
         }
         else if(userChoice==='scissors'){
             if (computerChoice==='lizard'){
                 score.textContent='Computer Won';
-               
             }else{
                 score.textContent='User Won';
                 userScore++;
-                userChoice++;userChoice;
             }
-}
+        }
         else if(userChoice==='lizard'){
             if (computerChoice==='spock'){
         score.textContent='Computer Won';
-        
             }else{
                 score.textContent='User Won';
                 userScore++;
-                userChoice++;userChoice;
-    }
-
-    
+                }
+        }
+        updateScore();
+        function updateScore(){
+            score.textContent=`User: ${userScore} - Computer: ${computerScore}`;
 }
-
-}
-
-updateScore();
-
-function updateScore(){
-    score.textContent=`User: ${userScore} - Computer: ${computerScore}`;
 }
