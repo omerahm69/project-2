1e3757356bf58260f0b916ebccaa76012cf4ba9a
omer ahmed <omerah69@gmail.co>
2024-06-19 21:21:22 +0200

    "fixining playgame function"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,77 @@
 const rock=document.getElementById('rock');
 const paper=document.getElementById('paper');
-const scissor=document.getElementById('scissor');
+const scissors=document.getElementById('scissors');
 const lizard=document.getElementById('lizard');
 const spock=document.getElementById('spock');
 
-const userChoices=[rock, paper, scissor, lizard, spock];
-const computerChoices=['rock','paper','scissor','lizard','spock'];
+const userChoices=[rock, paper, scissors, lizard, spock];
+const computerChoices=['rock','paper','scissors','lizard','spock'];
 
 const score=document.getElementById('score');
 
 let  userScore=0;
 let computerScore=0;
 
-userChoices.forEach((choice,index) => {
+userChoices.forEach((choice, index) => {
     choice.addEventListener('click', () => playGame(computerChoices[index]));
 });
 
 function playGame(userChoice){
     const random=Math.floor(Math.random() * computerChoices.length);
     const computerChoice= computerChoices[random];
+
     if (userChoice===computerChoice){
         score.textContent='its a draw';
         }
         else if(userChoice==='rock'){
-            if(computerChoice==='paper'){
+            if(computerChoice==='paper' || computerChoice==='spock' ){
                 score.textContent='Computer Won';
+                computerScore++;
             }else {
                 score.textContent='User Won';
                 userScore++;
             }
         }
         else if(userChoice==='paper'){
-            if (computerChoice==='scissors'){
+            if (computerChoice==='scissors' || computerChoice=='lizard'){
                 score.textContent='Computer Won';
+                computerScore++;
             }else{
                 score.textContent='User Won';
                 userScore++;
             }
         }
         else if(userChoice==='scissors'){
-            if (computerChoice==='lizard'){
+            if (computerChoice==='rock' || computerChoice==='spock'){
                 score.textContent='Computer Won';
+                computerScore++;
             }else{
                 score.textContent='User Won';
                 userScore++;
             }
         }
         else if(userChoice==='lizard'){
-            if (computerChoice==='spock'){
+            if (computerChoice==='rock' || computerChoice==='scissors'){
         score.textContent='Computer Won';
+        computerScore++;
             }else{
                 score.textContent='User Won';
                 userScore++;
                 }
         }
+        else if(userChoice==='spock'){
+            if (computerChoice==='paper' || computerChoice==='lizard'){
+        score.textContent='Computer Won';
+        computerScore++;
+            }else{
+                score.textContent='User Won';
+                userScore++;
+                }
+
+            }
+
         updateScore();
+    }
         function updateScore(){
             score.textContent=`User: ${userScore} - Computer: ${computerScore}`;
-}
 }
